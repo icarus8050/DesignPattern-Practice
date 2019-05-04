ea7fa2bd2ac002af0343336b52696baa99bafb9f
leafy <icarus8050@naver.com>
2019-05-04 17:53:03 +0900

    sample code

--- ProxyPattern/src/Main.java
+++ ProxyPattern/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        Image image_1 = new ProxyImage("image_1");
+        Image image_2 = new ProxyImage("image_2");
 
+        image_1.displayImage();
+        image_2.displayImage();
     }
 }
